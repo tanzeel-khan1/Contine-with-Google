c767aab48fb342b8ecfb322b7bfc5d1387c12bf1
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-02-14 00:41:12 +0500

    fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,45 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       sparse: true, // ✅ google users ke liye safe
-//     },
-
-//     password: {
-//       type: String,
-//       required: false, // ✅ IMPORTANT
-//       select: false,
-//     },
-
-//     googleId: {
-//       type: String,
-//       unique: true,
-//       sparse: true,
-//     },
-
-//     avatar: {
-//       type: String,
-//     },
-
-//     provider: {
-//       type: String,
-//       enum: ["local", "google"],
-//       default: "local",
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", userSchema);
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -53,7 +11,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      sparse: true, // google users ke liye safe
+      sparse: true, 
     },
 
     password: {
@@ -82,7 +40,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["admin", "student", "teacher", "user"],
-      default: "user", // by default student
+      default: "user", 
     },
   },
   { timestamps: true }
