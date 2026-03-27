c34be5d433c3d73b09d453de7d84d428e43a6c12
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-03-27 12:45:49 +0500

    fixes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
   let token;
-
   try {
     if (
       req.headers.authorization &&
--- frontend/src/admin/AdminDashboard.jsx
+++ frontend/src/admin/AdminDashboard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const AdminDashboard = () => {
   return
-   <div>admin dashaord</div>;
+   <div>a
+    dmin dashaord</div>;
 };
 
 export default AdminDashboard;
