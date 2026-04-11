0d675146f943c6da10bd6a273a59ae550eff7d84
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-04-11 12:17:21 +0500

    fixes

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
 const AdminLayout = () => {
+  
   return (
     
     <div className="min-h-screen bg-white">
